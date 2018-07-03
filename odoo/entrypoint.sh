7f8ab413a3c7601616a66e432e02a2d92d1b24f0
Fabrizio Arzeni <fabrizio.arzeni@metadonors.it>
2018-07-03 18:05:18 +0200

    fine

--- odoo/entrypoint.sh
+++ odoo/entrypoint.sh
@@ -5,8 +5,15 @@ args=("$@")
 /usr/bin/env python3 /mnt/utils/scripts/generate_odoo_conf.py --template='/mnt/utils/scripts/conf/odoo.cfg.j2' --config='/mnt/utils/scripts/conf/odoo_conf.yaml' > /etc/odoo/odoo.conf
 
 case $1 in
+    ipython)
+        exec ipython
+        ;;
     shell)
         exec odoo shell ${args[@]:1}
+        ;;
+    upgrade)
+        exec odoo -c /etc/odoo/odoo.conf -u ${args[@]:1} --stop-after-init
+        
         ;;
     run)
         exec odoo -c /etc/odoo/odoo.conf ${args[@]:1}
