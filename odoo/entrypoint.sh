91e5a70e8a8a6ee751fa27007e26a23eb8dfec8e
Fabrizio Arzeni <fabrizio.arzeni@metadonors.it>
2018-07-20 00:55:51 +0200

    add nginx, fix multiprocess

--- odoo/entrypoint.sh
+++ odoo/entrypoint.sh
@@ -3,20 +3,26 @@ set -e
 args=("$@")
 
 /usr/bin/env python3 /mnt/utils/scripts/generate_odoo_conf.py --template='/mnt/utils/scripts/conf/odoo.cfg.j2' --config='/mnt/utils/scripts/conf/odoo_conf.yaml' > /etc/odoo/odoo.conf
+echo "Starting nginx..."
+nginx -c /etc/nginx/nginx.conf &    
+
+
 
 case $1 in
     ipython)
-        exec ipython
+        exec sudo -u odoo ipython
         ;;
     shell)
-        exec odoo shell ${args[@]:1}
+        exec sudo -u odoo odoo shell ${args[@]:1}
         ;;
     upgrade)
-        exec odoo -c /etc/odoo/odoo.conf -u ${args[@]:1} --stop-after-init
+        exec sudo -u odoo odoo -c /etc/odoo/odoo.conf -u ${args[@]:1} --stop-after-init
         
         ;;
     run)
-        exec odoo -c /etc/odoo/odoo.conf ${args[@]:1}
+        
+        echo "Starting Odoo..."
+        sudo -u odoo odoo -c /etc/odoo/odoo.conf ${args[@]:1}
         ;;
     *)
         exec "$@"
