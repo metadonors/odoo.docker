b6cd3f7cbf1f07cecfbe5f60fb79dd7f1c887d59
Fabrizio Arzeni <fabrizio.arzeni@metadonors.it>
2021-06-29 12:57:32 +0200

    update dependecies

--- Dockerfile
+++ Dockerfile
@@ -2,38 +2,39 @@ FROM odoo:12.0
 
 USER root
 
-#RUN echo "deb http://ftp.de.debian.org/debian testing main" >> /etc/apt/sources.list
-#RUN echo 'APT::Default-Release "stable";' | tee -a /etc/apt/apt.conf.d/00local
+RUN mkdir -p /mnt/utils /mnt/bundle-addons \
+    && chown -R odoo:odoo /mnt/utils /mnt/bundle-addons
+
 RUN apt-get update \
     && apt-get install -y --no-install-recommends \
-        git \
         vim \
-        build-essential \
-        autoconf \
         libtool \
         nginx \
         sudo \
-        openssh-client \
         pkg-config \
-    && apt-get -y install python3 python3-dev python3-pip \
-    && mkdir -p /mnt/utils /mnt/bundle-addons \
-    && chown -R odoo:odoo /mnt/utils /mnt/bundle-addons
-
+        python3-dev \
+       # python3 python3-dev python3-pip 
+    && rm -rf /var/lib/apt/lists/*
 
 COPY ./odoo/extra-requirements.txt /mnt/utils/extra-requirements.txt
+
+RUN pip3 install -U wheel setuptools>=38.6.1
+RUN pip3 install -r /mnt/utils/extra-requirements.txt
+
 COPY ./odoo/scripts /mnt/utils/scripts
 COPY ./odoo/conf /mnt/utils/scripts/conf
-COPY ./odoo/entrypoint.sh /entrypoint.sh
-COPY ./odoo/wait-for-it.sh /wait-for-it.sh
+# COPY ./odoo/wait-for-it.sh /wait-for-it.sh
 
 COPY ./odoo/conf/web/nginx.conf /etc/nginx/nginx.conf
 COPY ./odoo/conf/web/odoo.conf /etc/nginx/conf.d/default.conf
 
 COPY ./addons/ /mnt/bundle-addons/
 
-RUN pip3 install wheel \
-    && pip3 install -r /mnt/utils/extra-requirements.txt
+COPY ./odoo/entrypoint.sh /entrypoint.sh
 
+
+# CMD ["run"]
+ENTRYPOINT ["/entrypoint.sh"]
 CMD ["run"]
 
 
--- odoo/entrypoint.sh
+++ odoo/entrypoint.sh
@@ -4,29 +4,54 @@ args=("$@")
 
 /usr/bin/env python3 /mnt/utils/scripts/generate_odoo_conf.py --template='/mnt/utils/scripts/conf/odoo.cfg.j2' --config='/mnt/utils/scripts/conf/odoo_conf.yaml' > /etc/odoo/odoo.conf
 
+# set the postgres database host, port, user and password according to the environment
+# and pass them as arguments to the odoo process if not present in the config file
+: ${HOST:=${ODOO_DB_HOST:='db'}}
+: ${PORT:=${ODOO_DB_PORT:='5432'}}
+: ${USER:=${ODOO_DB_USER:=${POSTGRES_USER:='odoo'}}}
+: ${PASSWORD:=${ODOO_DB_PASSWORD:=${POSTGRES_PASSWORD:='odoo'}}}
+
+DB_ARGS=()
+function check_config() {
+    param="$1"
+    value="$2"
+    # if grep -q -E "^\s*\b${param}\b\s*=" "$ODOO_RC" ; then       
+    #     value=$(grep -E "^\s*\b${param}\b\s*=" "$ODOO_RC" |cut -d " " -f3|sed 's/["\n\r]//g')
+    # fi;
+    DB_ARGS+=("--${param}")
+    DB_ARGS+=("${value}")
+}
+
+check_config "db_host" "$HOST"
+check_config "db_port" "$PORT"
+check_config "db_user" "$USER"
+check_config "db_password" "$PASSWORD"
+
 case $1 in
     ipython)
-        exec sudo -E -u odoo /wait-for-it.sh postgres:5432 -- odoo ipython
+        exec sudo -E -u odoo odoo ipython
         ;;
     shell)
-        exec sudo -E -u odoo /wait-for-it.sh postgres:5432 -- odoo shell ${args[@]:1}
+        exec sudo -E -u odoo odoo shell ${args[@]:1}
         ;;
     upgrade)
-        exec sudo -E -u odoo /wait-for-it.sh postgres:5432 -- odoo -c /etc/odoo/odoo.conf -u ${args[@]:1} --stop-after-init
+        exec sudo -E -u odoo odoo -c /etc/odoo/odoo.conf -u ${args[@]:1} --stop-after-init
         ;;
     test)
-        exec sudo -E -u odoo /wait-for-it.sh postgres:5432 -- odoo -c /etc/odoo/odoo.conf --workers 0 --log-level=test --test-enable --stop-after-init -u ${args[@]:1}
+        exec sudo -E -u odoo odoo -c /etc/odoo/odoo.conf --workers 0 --log-level=test --test-enable --stop-after-init -u ${args[@]:1}
         ;;
     test_all)
-        exec sudo -E -u odoo /wait-for-it.sh postgres:5432 -- odoo -c /etc/odoo/odoo.conf --workers 0 --test-enable --stop-after-init
+        exec sudo -E -u odoo odoo -c /etc/odoo/odoo.conf --workers 0 --test-enable --stop-after-init
         ;;
     run)
+        wait-for-psql.py ${DB_ARGS[@]} --timeout=30
         echo "Starting nginx..."
         nginx -c /etc/nginx/nginx.conf &    
         echo "Starting Odoo..."
-        sudo -E -u odoo /wait-for-it.sh postgres:5432 -- odoo -c /etc/odoo/odoo.conf ${args[@]:1}
+        sudo -E -u odoo odoo -c /etc/odoo/odoo.conf ${args[@]:1}
         ;;
     prod)
+        wait-for-psql.py ${DB_ARGS[@]} --timeout=30
         echo "Starting nginx..."
         exec nginx -c /etc/nginx/nginx.conf &    
 
